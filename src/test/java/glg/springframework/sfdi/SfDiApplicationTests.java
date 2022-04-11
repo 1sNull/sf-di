85e8417132dce31a6ad5703ca845b1567e8ffa51
1sNull <93542640+1sNull@users.noreply.github.com>
2022-04-11 14:54:32 -0400

    first commit

---
+++ src/test/java/glg/springframework/sfdi/SfDiApplicationTests.java
@@ -0,0 +1,13 @@
+package glg.springframework.sfdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
